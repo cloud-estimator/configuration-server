51c6e614107469926a542e440c75c4886d5bc92b
Dharminder Dharna <dharminder@stellar.aero>
2018-03-15 16:15:32 -0700

    - updates tag

--- travis_scripts/deploy_to_docker_hub.sh
+++ travis_scripts/deploy_to_docker_hub.sh
@@ -1,3 +1,3 @@
 echo "Pushing service docker images to docker hub ...."
 docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
-docker push cloudestimator/configuration-server
+docker push cloud-estimator/configuration-server:$BUILD_NAME
