3aae53a6a46ef4c742f48f3f0bf04c93e78b285c
Dharminder Dharna <dharminder@stellar.aero>
2018-03-15 16:30:43 -0700

    - adds tagging

--- travis_scripts/deploy_to_docker_hub.sh
+++ travis_scripts/deploy_to_docker_hub.sh
@@ -1,3 +1,4 @@
 echo "Pushing service docker images to docker hub ...."
 docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
-docker push cloud-estimator/configuration-server:latest
+docker tag cloud-estimator/configuration-server cloudestimator/configuration-server:$BUILD_NAME
+docker push cloudestimator/configuration-server:$BUILD_NAME
